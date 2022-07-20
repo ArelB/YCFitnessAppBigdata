4c71edecf3b5884782050a7dc19d930a664a6db3
FelixvL <digitaleservice@gmail.com>
2022-07-20 16:33:15 +0200

    flask werkend

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import Felix
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!!</p>"
+
+@app.route("/tweede")
+def andere():
+    return Felix.vanFelix1()
