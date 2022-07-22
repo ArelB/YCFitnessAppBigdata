f8b4a73dc94c4ed3881ab57adc3ae1ef3a8a1236
WouterHilberink <w.h.hilberink@hotmail.nl>
2022-07-22 15:46:39 +0200

    hoofdletter fuck you

--- Wouter.py
+++ Wouter.py
@@ -1,12 +1,12 @@
 import pandas
 
 def csvFunctie():
-    df = pandas.read_csv("pokemon.csv")    
+    df = pandas.read_csv("Pokemon.csv")    
     #flask.render_template("test_template.html")
     return str(df.columns)
 
 def printPokemon(pok):
-    df = pandas.read_csv("pokemon.csv") 
+    df = pandas.read_csv("Pokemon.csv") 
     return pok
 
 # def hello(name=None):
