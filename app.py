010c3d2f0649ace8ae2212d29bed9e2a1b3b31f6
FelixvL <digitaleservice@gmail.com>
2022-08-04 10:53:40 +0200

    cors erbij

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 import Felix
 import Wouter
 import VoortgangVisualisatie
 import RunTipsScraping
 
-
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@cross_origin()
 def hello_world():
     return "<p>Hello, World!!</p>"
 
@@ -28,6 +31,7 @@ def csvStijl():
 
 #return basic colomns in json format
 @app.route("/csvJSON")
+@cross_origin()
 def csvJSON():
     return Wouter.csvJSON()
 
