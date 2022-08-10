736d8fa7e403ded9432ff8610cffecff0fb25649
WouterHilberink <w.h.hilberink@hotmail.nl>
2022-08-10 13:56:40 +0200

    T29 afmaken

--- VoortgangVisualisatie.py
+++ VoortgangVisualisatie.py
@@ -3,10 +3,7 @@ import plotly.express as px
 import pandas as pd
 import datetime
 
-def printhome():
-    return "hey"
-
-def ShowGraph(persoon):
+def ShowGraph(persoon, route):
 
     #bepalen wat meest recente bestand is:
     server = 'yc2207hardloopserver.database.windows.net'
@@ -19,20 +16,47 @@ def ShowGraph(persoon):
         with conn.cursor() as cursor:
             # cursor.execute("SELECT R.DateAndTime, R.Duration FROM dbo.[User] U INNER JOIN Run R ON R.UserID = U.ID WHERE U.UserName = '"+persoon+"' AND RouteID = 9")
             # row = cursor.fetchall()
-            sql_query = pd.read_sql_query("SELECT R.DateAndTime, R.Duration FROM dbo.[User] U INNER JOIN Run R ON R.UserID = U.ID WHERE U.UserName = '"+persoon+"' AND RouteID = 9", 
-            conn, parse_dates={"DateAndTime": {"format": "%Y%M%d %H%M"}, "Duration": {"format": "%H%M"}})
+            Users = pd.read_sql_query("SELECT ID, UserName FROM dbo.[User]", conn)
+            Routes = pd.read_sql_query("SELECT ID, Name FROM dbo.[Route]", conn)
 
+            wherepersoon = "WHERE U.UserName = '"+persoon
+            whereroute = "' AND RouteID = "+route
+
+            if persoon in Users["UserName"].unique():
+                wherepersoon = "WHERE U.UserName = '"+persoon
+            elif int(persoon) in Users["ID"].unique():
+                wherepersoon = "WHERE U.ID = '"+persoon
+            else: return "De waarde die u heeft opgegeven voor gebruiker komt niet overeen met de naam of id van een gebruiker"
+
+            if str(route) in Routes["Name"].unique():
+                whereroute = "' AND RouteID = '"+ str(Routes.loc[Routes["Name"] == route, "ID"].iloc[0])
+            else:
+                try:
+                    if int(route) in Routes["ID"].unique():
+                        whereroute = "' AND RouteID = '"+route
+                    else: return "De waarde die u heeft opgegeven voor route komt niet overeen met de naam of id van een route"
+                except: return "De waarde die u heeft opgegeven voor route komt niet overeen met de naam of id van een route"
+
+            wherestatement = wherepersoon + whereroute
+            try:
+                sql_query = pd.read_sql_query("SELECT R.DateAndTime, R.Duration FROM dbo.[User] U INNER JOIN Run R ON R.UserID = U.ID "+wherestatement+"'", 
+                conn, parse_dates={"DateAndTime": {"format": "%Y%M%d %H%M"}, "Duration": {"format": "%H%M"}})
+
+                df = pd.DataFrame(sql_query, columns=["DateAndTime", "Duration"])
+            except:
+                return "de combinatie van deze gebruiker en deze route heeft geen runs"
             
-            df = pd.DataFrame(sql_query, columns=["DateAndTime", "Duration"])
-
     df["Durationsec"] = 0
+
+    print(Users)
+    print(Routes)
 
     for idx, x in enumerate(df["Duration"]):
         df["Durationsec"].iloc[idx] = (x.hour * 60 + x.minute) * 60 + x.second    
     
     print(df)
     df = df.sort_values(by="DateAndTime")
-    fig = px.line(df, x="DateAndTime", y="Durationsec", title="Tijd route 9 van "+persoon, markers=True)
+    fig = px.line(df, x="DateAndTime", y="Durationsec", title="Tijd route "+route+" van "+persoon, markers=True)
     fig.show()
     
     # print("---------============== einde execute")
